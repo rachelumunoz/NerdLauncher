d403354d79055500d612c93c3af9789605701e31
Rachel Munoz <rachelumunoz@gmail.com>
2017-08-04 22:02:19 -0700

    Adds setupAdapter to query system for matching Activities

--- app/src/main/java/io/rachelmunoz/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/io/rachelmunoz/nerdlauncher/NerdLauncherFragment.java
@@ -1,19 +1,27 @@
 package io.rachelmunoz.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by rachelmunoz on 8/4/17.
  */
 
 public class NerdLauncherFragment extends Fragment {
+	private static final String TAG = "NerdLauncherFragment";
+
 	private RecyclerView mRecyclerView;
 
 	public static NerdLauncherFragment newInstance(){
@@ -28,6 +36,18 @@ public class NerdLauncherFragment extends Fragment {
 		mRecyclerView = (RecyclerView) v.findViewById(R.id.app_recycler_view);
 		mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+		setupAdapter();
 		return v;
+	}
+
+	private void setupAdapter(){
+		Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+		startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		PackageManager pm = getActivity().getPackageManager();
+		List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+		Log.i(TAG, "Found " + activities.size() + " activities");
+
 	}
 }
